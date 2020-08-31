144ecfb21c8ed00e8e3073dff3435a87d1bfd3fd
ThibaultTricard <thibault.tricard@hotmail.fr>
2020-08-31 16:16:27 +0200

    Added an example of compute pipeline used doing a fourier transform

--- Samples/Data/Shaders/FourierTransform/shader.frag
+++ Samples/Data/Shaders/FourierTransform/shader.frag
@@ -11,8 +11,15 @@ layout( location = 0 ) in vec2 texcoord;
 layout( location = 0 ) out vec4 frag_color;
 
 void main() {
-  vec2 coord = texcoord * vec2(sizeX, sizeY);
+  vec2 coord = texcoord - vec2(0.5);
+  if (coord.x < 0.0){
+  	coord.x+= 1.0;
+  }
+  if (coord.y < 0.0){
+  	coord.y+= 1.0;
+  }
+  coord = coord  * vec2(sizeX, sizeY) ;
   float r = imageLoad(transform,int(int(coord.x) + int(coord.y) * sizeX)).x ;
   float i = imageLoad(transform,int(int(coord.x) + int(coord.y) * sizeX + sizeX* sizeY)).x;
-  frag_color = vec4(sqrt(r*r+i*i));
+  frag_color = vec4(sqrt(r*r+i*i)/1000.0);
 }
--- Samples/Source Files/FourierTransform/main.cpp
+++ Samples/Source Files/FourierTransform/main.cpp
@@ -5,32 +5,46 @@ using namespace LavaCake;
 
 int main() {
 
-	/*int nbFrames = 3;
+	int nbFrames = 3;
 	Framework::ErrorCheck::PrintError(true);
-	Framework::Window w("LavaCake : Post Process", 0, 0, 512, 512);
+	Framework::Window w("LavaCake : Fourier Transform", 512, 512);
 
 	Framework::Device* d = Framework::Device::getDevice();
 	d->initDevices(1, 1, w.m_windowParams);
-	d->prepareFrames();
+	LavaCake::Framework::SwapChain* s = LavaCake::Framework::SwapChain::getSwapChain();
+	s->init();
 
+	VkQueue queue = d->getGraphicQueue(0)->getHandle();
+	VkQueue& present_queue = d->getPresentQueue()->getHandle();
+
+	VkQueue& compute_queue = d->getComputeQueue(0)->getHandle();
+
+	VkDevice logical = d->getLogicalDevice();
+	VkExtent2D size = s->size();
+
+	std::vector<Framework::CommandBuffer> commandBuffer = std::vector<Framework::CommandBuffer>(nbFrames);
+	for (int i = 0; i < nbFrames; i++) {
+		commandBuffer[i].addSemaphore();
+		commandBuffer[i].addSemaphore();
+	}
 
 	//PostProcessQuad
 	Helpers::Mesh::Mesh* quad = new Helpers::Mesh::Mesh();
 	Helpers::Mesh::preparePostProcessQuad(*quad,true);
 	Framework::VertexBuffer* quad_vertex_buffer = new Framework::VertexBuffer({ quad }, { 3,2 });
-	quad_vertex_buffer->allocate(*d->getPresentQueue(), d->getFrameRessources()->front().commandBuffer);
+	quad_vertex_buffer->allocate(queue, commandBuffer[0].getHandle());
 
 	//texture map
 	Framework::TextureBuffer* input = new Framework::TextureBuffer("Data/Textures/mandrill.png", 4);
-	input->allocate();
+	input->allocate(queue, commandBuffer[0].getHandle());
 
 
 	Framework::TexelBuffer* output_pass1 = new Framework::TexelBuffer();
 	std::vector<float> rawdata = std::vector<float>(input->width() * input->height() * 2);
-	output_pass1->allocate(rawdata, uint32_t(1));
+	output_pass1->allocate(queue, commandBuffer[0].getHandle(),rawdata, uint32_t(1));
 
 	Framework::TexelBuffer* output_pass2 = new Framework::TexelBuffer();
-	output_pass2->allocate(rawdata, uint32_t(1));
+	output_pass2->allocate(queue, commandBuffer[0].getHandle(),rawdata, uint32_t(1));
 
 	Framework::UniformBuffer* sizeBuffer = new Framework::UniformBuffer();
 	sizeBuffer->addVariable("width", input->width());
@@ -67,7 +81,7 @@ int main() {
 	//renderPass
 	Framework::RenderPass* showPass = new Framework::RenderPass();
 
-	Framework::GraphicPipeline* pipeline = new Framework::GraphicPipeline({ 0,0,0 }, { float(w.m_windowSize[0]),float(w.m_windowSize[1]),1.0f }, { 0,0 }, { float(w.m_windowSize[0]),float(w.m_windowSize[1]) });
+	Framework::GraphicPipeline* pipeline = new Framework::GraphicPipeline({ 0,0,0 }, { float(size.width),float(size.height),1.0f }, { 0,0 }, { float(size.width),float(size.height) });
 	Framework::VertexShaderModule* vertexShader = new Framework::VertexShaderModule("Data/Shaders/FourierTransform/shader.vert.spv");
 	Framework::FragmentShaderModule* fragmentShader = new Framework::FragmentShaderModule("Data/Shaders/FourierTransform/shader.frag.spv");
 	pipeline->setVextexShader(vertexShader);
@@ -83,85 +97,94 @@ int main() {
 	showPass->compile();
 
 
-	VkDevice logical = d->getLogicalDevice();
-	VkQueue& compute_queue = d->getComputeQueue(0)->getHandle();
-	Buffer::FrameResources& frame = d->getFrameRessources()->at(0);
-	VkCommandBuffer commandbuffer = frame.commandBuffer;
 
-	InitVkDestroyer(logical, frame.framebuffer);
-	if (!Command::BeginCommandBufferRecordingOperation(commandbuffer, VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT, nullptr)) {
-		
+
+	std::vector<Framework::FrameBuffer*> frameBuffers;
+	for (int i = 0; i < nbFrames; i++) {
+		frameBuffers.push_back(new Framework::FrameBuffer(s->size().width, s->size().height));
+		showPass->prepareOutputFrameBuffer(*frameBuffers[i]);
 	}
 
 
-	sizeBuffer->update(commandbuffer);
+	commandBuffer[0].wait(2000000000);
+	commandBuffer[0].resetFence();
+	commandBuffer[0].beginRecord();
 
-	computePipeline1->compute(commandbuffer, input->width(), input->height(), 1);
+	sizeBuffer->update(commandBuffer[0].getHandle());
 
+	computePipeline1->compute(commandBuffer[0].getHandle(), input->width(), input->height(), 1);
 
-
-	VkSemaphoreCreateInfo semaphoreInfo = {
-		VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
-		nullptr,
-		0
-	};
-
-	VkSemaphore semaphorePass1;
-	VkResult r = LavaCake::vkCreateSemaphore(logical, &semaphoreInfo, nullptr,&semaphorePass1);
-	std::vector<Semaphore::WaitSemaphoreInfo> wait_semaphore_infos = {};
-	wait_semaphore_infos.push_back({
-		semaphorePass1,
-		VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT
-		});
-
-
-	if (!LavaCake::Command::EndCommandBufferRecordingOperation(commandbuffer)) {
-
-	}
-
-	if (!Command::SubmitCommandBuffersToQueue(compute_queue, {}, { commandbuffer }, { semaphorePass1 }, {})) {
-	}
-
-
-	InitVkDestroyer(logical, frame.framebuffer);
-	if (!Command::BeginCommandBufferRecordingOperation(commandbuffer, VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT, nullptr)) {
-		
-	}
-
-
-	sizeBuffer->update(commandbuffer);
-
-	computePipeline1->compute(commandbuffer, input->width(), input->height(), 1);
-
-
-
-	VkSemaphoreCreateInfo semaphoreInfo = {
-		VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
-		nullptr,
-		0
-	};
-
-	VkSemaphore semaphorePass1;
-	VkResult r = LavaCake::vkCreateSemaphore(logical, &semaphoreInfo, nullptr,&semaphorePass1);
 	
 
 
-	if (!LavaCake::Command::EndCommandBufferRecordingOperation(commandbuffer)) {
+	commandBuffer[0].endRecord();
 
+	if (!Command::SubmitCommandBuffersToQueue(compute_queue, { }, { commandBuffer[0].getHandle() }, { }, { commandBuffer[0].getFence() })) {
 	}
 
-	if (!Command::SubmitCommandBuffersToQueue(compute_queue, {}, { commandbuffer }, { semaphorePass1 }, {})) {
+
+
+
+	commandBuffer[0].wait(2000000000);
+	commandBuffer[0].resetFence();
+	commandBuffer[0].beginRecord();
+
+	computePipeline2->compute(commandBuffer[0].getHandle(), input->width(), input->height(), 1);
+
+
+	commandBuffer[0].endRecord();
+
+
+	if (!Command::SubmitCommandBuffersToQueue(compute_queue, {}, { commandBuffer[0].getHandle() }, { commandBuffer[0].getSemaphore(1) }, { commandBuffer[0].getFence()})) {
 	}
 
-	std::vector<Semaphore::WaitSemaphoreInfo> wait_semaphore_infos = {};
-	wait_semaphore_infos.push_back({
-		semaphorePass1,
-		VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT
-		});
 
 
-	w.Show();
+
+	int f = 0;
+	while (w.running()) {
+		w.UpdateInput();
+		f++;
+		f = f % nbFrames;
 
 
-  return 0;*/
+		Framework::SwapChainImage& image = s->AcquireImage();
+
+		std::vector<LavaCake::Semaphore::WaitSemaphoreInfo> wait_semaphore_infos = {};
+		wait_semaphore_infos.push_back({
+			image.getSemaphore(),                     // VkSemaphore            Semaphore
+			VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT					// VkPipelineStageFlags   WaitingStage
+			});
+
+
+		commandBuffer[f].wait(2000000000);
+		commandBuffer[f].resetFence();
+		commandBuffer[f].beginRecord();
+
+
+		showPass->setSwapChainImage(*frameBuffers[f], image);
+
+
+		showPass->draw(commandBuffer[f].getHandle(), frameBuffers[f]->getFrameBuffer(), { 0,0 }, { size.width, size.height }, { { 0.1f, 0.2f, 0.3f, 1.0f }, { 1.0f, 0 } });
+
+		commandBuffer[f].endRecord();
+
+
+		if (!Command::SubmitCommandBuffersToQueue(queue, wait_semaphore_infos, { commandBuffer[f].getHandle() }, { commandBuffer[f].getSemaphore(0) }, commandBuffer[f].getFence())) {
+			continue;
+		}
+
+		Presentation::PresentInfo present_info = {
+			s->getHandle(),                                    // VkSwapchainKHR         Swapchain
+			image.getIndex()                                   // uint32_t               ImageIndex
+		};
+		if (!Presentation::PresentImage(present_queue, { commandBuffer[f].getSemaphore(0) }, { present_info })) {
+			continue;
+		}
+	}
+
+	d->end();
+
+
+  return 0;
 }
